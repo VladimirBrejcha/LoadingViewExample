9cdb3432683c50d4a7ab4afe97261231eb296d50
VladimirBrejcha <vladimirbrejcha@icloud.com>
2020-03-22 12:54:33 +0300

    Initial Commit

---
+++ LoadingViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadingViewExample
+//
+//  Created by Владимир Королев on 22.03.2020.
+//  Copyright © 2020 Vladimir Brejcha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
